32a33263707b95000c61d7a4a650c0e0284c512a
Aleksei Popryadukhin <aleksey.p10n@gmail.com>
2018-12-02 20:03:21 +0300

    admin server

---
+++ admin-server/src/main/java/com/tictactoe/adminserver/AdminServerApplication.java
@@ -0,0 +1,12 @@
+package com.tictactoe.adminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AdminServerApplication.class, args);
+  }
+}
