27e8f42ef53f9d5deb197013b704775ab6223767
Aleksei Popryadukhin <aleksey.p10n@gmail.com>
2018-12-03 10:04:41 +0300

    fix build

--- tictactoe-services/auth-module/src/main/java/com/tictactoe/authmodule/config/SpringSecurityWebFluxConfig.java
+++ tictactoe-services/auth-module/src/main/java/com/tictactoe/authmodule/config/SpringSecurityWebFluxConfig.java
@@ -1,75 +1,75 @@
-package com.tictactoe.authmodule.config;
-
-import com.tictactoe.authmodule.auth.JWTAuthSuccessHandler;
-import com.tictactoe.authmodule.service.JWTService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
-
-@Configuration
-//@EnableWebFluxSecurity
-@EnableReactiveMethodSecurity
-@ComponentScan("com.tictactoe.authmodule")
-public class SpringSecurityWebFluxConfig {
-
-  private static final String[] WHITELISTED_AUTH_URLS = {
-      "/login",
-      "/",
-      "/public/**",
-      "/auth/**"
-  };
-
-  /**
-   * The test defined in SampleApplicationTests class will only get executed
-   * if you change the authentication mechanism to basic (from form mechanism)
-   * in SpringSecurityWebFluxConfig file
-   *
-   * @param http
-   * @return
-   * @throws Exception
-   */
-  @Bean
-  public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, JWTService jwtService) {
-
-    AuthenticationWebFilter authenticationJWT = new AuthenticationWebFilter(new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsRepository()));
-    authenticationJWT.setAuthenticationSuccessHandler(new JWTAuthSuccessHandler(jwtService));
-
-    http.csrf().disable();
-
-    http
-        .authorizeExchange()
-        .pathMatchers(WHITELISTED_AUTH_URLS)
-        .permitAll()
-        .and()
-        .addFilterAt(authenticationJWT, SecurityWebFiltersOrder.FIRST)
-        .exceptionHandling()
-        .and()
-        .authorizeExchange()
-        .pathMatchers("/actuator/**").hasRole("ADMIN")
-        .anyExchange()
-        .authenticated()
-        .and()
-        .addFilterAt(new CommonJWTAuthWebFilter(jwtService), SecurityWebFiltersOrder.HTTP_BASIC)
-        .exceptionHandling();
-
-    return http.build();
-  }
-
-  @Bean
-  public MapReactiveUserDetailsService userDetailsRepository() {
-    UserDetails anonymous = User.withUsername("anonymous").password("{noop}secret").roles("GUEST").build();
-    UserDetails user = User.withUsername("user").password("{noop}password").roles("USER").build();
-    UserDetails admin = User.withUsername("admin").password("{noop}password").roles("USER", "ADMIN").build();
-    return new MapReactiveUserDetailsService(anonymous, user, admin);
-  }
-
-}
+//package com.tictactoe.authmodule.config;
+//
+//import com.tictactoe.authmodule.auth.JWTAuthSuccessHandler;
+//import com.tictactoe.authmodule.service.JWTService;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
+//import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+//import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+//
+//@Configuration
+////@EnableWebFluxSecurity
+//@EnableReactiveMethodSecurity
+//@ComponentScan("com.tictactoe.authmodule")
+//public class SpringSecurityWebFluxConfig {
+//
+//  private static final String[] WHITELISTED_AUTH_URLS = {
+//      "/login",
+//      "/",
+//      "/public/**",
+//      "/auth/**"
+//  };
+//
+//  /**
+//   * The test defined in SampleApplicationTests class will only get executed
+//   * if you change the authentication mechanism to basic (from form mechanism)
+//   * in SpringSecurityWebFluxConfig file
+//   *
+//   * @param http
+//   * @return
+//   * @throws Exception
+//   */
+//  @Bean
+//  public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, JWTService jwtService) {
+//
+//    AuthenticationWebFilter authenticationJWT = new AuthenticationWebFilter(new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsRepository()));
+//    authenticationJWT.setAuthenticationSuccessHandler(new JWTAuthSuccessHandler(jwtService));
+//
+//    http.csrf().disable();
+//
+//    http
+//        .authorizeExchange()
+//        .pathMatchers(WHITELISTED_AUTH_URLS)
+//        .permitAll()
+//        .and()
+//        .addFilterAt(authenticationJWT, SecurityWebFiltersOrder.FIRST)
+//        .exceptionHandling()
+//        .and()
+//        .authorizeExchange()
+//        .pathMatchers("/actuator/**").hasRole("ADMIN")
+//        .anyExchange()
+//        .authenticated()
+//        .and()
+//        .addFilterAt(new CommonJWTAuthWebFilter(jwtService), SecurityWebFiltersOrder.HTTP_BASIC)
+//        .exceptionHandling();
+//
+//    return http.build();
+//  }
+//
+//  @Bean
+//  public MapReactiveUserDetailsService userDetailsRepository() {
+//    UserDetails anonymous = User.withUsername("anonymous").password("{noop}secret").roles("GUEST").build();
+//    UserDetails user = User.withUsername("user").password("{noop}password").roles("USER").build();
+//    UserDetails admin = User.withUsername("admin").password("{noop}password").roles("USER", "ADMIN").build();
+//    return new MapReactiveUserDetailsService(anonymous, user, admin);
+//  }
+//
+//}
--- tictactoe-services/user-service/src/main/java/com/tictactoe/userservice/config/ApplicationConfig.java
+++ tictactoe-services/user-service/src/main/java/com/tictactoe/userservice/config/ApplicationConfig.java
@@ -1,7 +1,6 @@
 
 package com.tictactoe.userservice.config;
 
-import com.tictactoe.authmodule.config.SpringSecurityWebFluxConfig;
 import com.tictactoe.authmodule.config.SpringWebFluxConfig;
 import lombok.Data;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +13,7 @@ import org.springframework.context.annotation.Import;
  */
 @Data
 @Configuration
-@Import({SpringSecurityWebFluxConfig.class, SpringWebFluxConfig.class})
+@Import({SpringWebFluxConfig.class})
 public class ApplicationConfig {
 
 }
