395ae31cced599247daa54fd04de0a7e03ed63a2
Aleksei Popryadukhin <aleksey.p10n@gmail.com>
2018-12-06 21:27:13 +0300

    fix run.sh

--- spring-servers/config-server/docker/run.sh
+++ spring-servers/config-server/docker/run.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
+getPort() {
+    echo $1 | cut -d : -f 3 | xargs basename
+}
+
 echo "********************************************************"
-echo "Starting the Eureka Server"
+echo "Waiting for the eureka server to start on port $(getPort $DISCOVERY_SERVER_PORT)"
+echo "********************************************************"
+while ! `nc -z discoveryserver  $(getPort DISCOVERY_SERVER_PORT)`; do sleep 3; done
+echo "******* Eureka Server has started"
+
+echo "********************************************************"
+echo "Starting Config Server"
 echo "********************************************************"
 /app.jar
