2d114f952f10b1a85f8c69e28eda0dcf5e8f730c
Aleksey <aleksey.popryaduhin@yahoo.com>
2018-12-07 04:08:36 +0000

    scripts

---
+++ create-images.sh
@@ -0,0 +1,4 @@
+./gradlew :spring-servers:discovery-server:docker
+./gradlew :spring-servers:config-server:docker
+./gradlew :spring-servers:gateway-server:docker
+./gradlew :spring-servers:admin-server:docker
