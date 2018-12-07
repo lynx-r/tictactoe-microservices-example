320e25ee0410fb551988083f797b3500a26899b2
Aleksei Popryadukhin <aleksey.p10n@gmail.com>
2018-12-07 07:42:27 +0300

    😀

--- create-images.sh
+++ create-images.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+./gradlew clean build -x test
 ./gradlew :spring-servers:discovery-server:docker
 ./gradlew :spring-servers:config-server:docker
 ./gradlew :spring-servers:gateway-server:docker
