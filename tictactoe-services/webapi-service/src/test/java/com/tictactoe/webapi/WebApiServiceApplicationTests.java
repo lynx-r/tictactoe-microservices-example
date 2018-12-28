c745e700065a7b2b6e499d1fd1f77a72ca445117
lynx-r <aleksey.p10n@gmail.com>
2018-12-28 08:35:49 +0300

    tests

--- tictactoe-services/webapi-service/src/test/java/com/tictactoe/webapi/WebApiServiceApplicationTests.java
+++ tictactoe-services/webapi-service/src/test/java/com/tictactoe/webapi/WebApiServiceApplicationTests.java
@@ -26,6 +26,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.reactive.function.client.WebClient;
 
+/**
+ * sorry tests outdated 😢
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WebApiServiceApplicationTests {
